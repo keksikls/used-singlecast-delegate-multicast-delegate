36fa3992b10991735e0a7621557382003cd6f9b9
keksikls <151632212+keksikls@users.noreply.github.com>
2024-11-02 04:56:11 +0300

    used-singlecast-delegate-multicast-delegate
    used-singlecast-delegate-multicast-delegate

---
+++ Multicast delegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace single_delegate_multi_delegate
+{
+    class Multicast_delegate
+    {
+        public delegate void NoParametrs();
+
+        static void Main(string[] args) 
+        {
+            NoParametrs NoPar = new(ShowMassage_1);
+
+            // multi cast delegated
+            NoPar += ShowMassage_2;
+
+            NoPar();
+
+            Console.ReadLine();
+        }
+
+        private static void ShowMassage_1() => Console.WriteLine("Im used");
+
+        private static void ShowMassage_2() => Console.WriteLine("Im used two");
+
+    }
+}
---
+++ singlecast delegate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace single_delegate_multi_delegate
+{
+    internal class singlecast_delegate
+    {
+        class Program
+        {
+            public delegate string Parametrs(string name_1, string name_2);
+
+            static void Main(string[] args)
+            {
+                //ссылается на 1 метод
+                Parametrs MyParam = new(WriteMassage);
+
+                string value = MyParam("Ejik", "Krosh");
+
+                Console.WriteLine(value);
+
+                Console.ReadLine();
+            }
+
+            private static string WriteMassage(string name_1, string name_2)
+            {
+                Console.WriteLine($"{name_1} , {name_2} - using delegate great");
+                return "from me";
+            }
+        }
+    }
+}
